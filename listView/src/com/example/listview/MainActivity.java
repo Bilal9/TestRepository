b1c929d78f6be60b87cb77ce1e7ad934fafda302
Bilal9 <bilal.qa97@gmail.com>
2013-11-10 17:36:38 -0800

    list

--- listView/src/com/example/listview/MainActivity.java
+++ listView/src/com/example/listview/MainActivity.java
@@ -3,25 +3,34 @@ package com.example.listview;
 import android.R.string;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	ListView listview; 
-	
+	String[] planets;
+	Context context;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		context=this;
 		listview=(ListView) findViewById(R.id.listView);
 		
 		
-		String [] planets=this.getResources().getStringArray(R.array.Planetsarray); 
+	 planets=this.getResources().getStringArray(R.array.Planetsarray); 
 		
 		ArrayAdapter<String> Adapter = new ArrayAdapter<String>(this,R.layout.listviewrow,R.id.TextView, planets);
 		listview.setAdapter(Adapter);
+		listview.setOnItemClickListener(new ClickListHander());
 	}
 
 	@Override
@@ -31,4 +40,18 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	
+	private class ClickListHander implements OnItemClickListener 
+	{
+		
+		@Override
+		public void onItemClick(AdapterView<?> arg0, View view, int position,long arg3) {
+			TextView listtext = (TextView) view.findViewById(R.id.TextView);
+			String text=listtext.getText().toString();
+			
+			Toast.makeText(context, text+"Click at position "+position, Toast.LENGTH_SHORT).show();
+			
+		}
+		
+	}
 }
